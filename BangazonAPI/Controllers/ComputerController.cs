1d986416e96ffbebcc9bce3f603140eff148a656
Jasmine <jasminewilliams2017@gmail.com>
2020-04-02 12:09:32 -0500

    WIP

--- BangazonAPI/Controllers/ComputerController.cs
+++ BangazonAPI/Controllers/ComputerController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonAPI.Controllers
 {
@@ -11,5 +14,214 @@ namespace BangazonAPI.Controllers
     [ApiController]
     public class ComputerController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
+        public ComputerController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        //Get all computers from the database
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, PurchaseDate, DecomissionDate, Make, Model FROM Computer";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Computer> computers = new List<Computer>();
+
+                    while (reader.Read())
+                    {
+                        Computer computer = new Computer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                            DecomissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate")),
+                            Make = reader.GetString(reader.GetOrdinal("Make")),
+                            Model = reader.GetString(reader.GetOrdinal("Model"))
+                        };
+
+                        computers.Add(computer);
+                    }
+                    reader.Close();
+
+                    return Ok(computers);
+                }
+            }
+        }
+
+
+        //Get a single computer by Id
+        [HttpGet("{id}", Name = "GetComputer")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id, PurchaseDate, DecomissionDate, Make, Model
+                        FROM Computer
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Computer computer = null;
+
+                    if (reader.Read())
+                    {
+                        computer = new Computer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                            DecomissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate")),
+                            Make = reader.GetString(reader.GetOrdinal("Make")),
+                            Model = reader.GetString(reader.GetOrdinal("Model"))
+                        };
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                    reader.Close();
+
+                    return Ok(computer);
+                }
+            }
+        }
+
+
+        ////Create a new computer
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Computer computer)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Computer (PurchaseDate, DecomissionDate, Make, Model)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@PurchaseDate, @DecomissionDate, @Make, @Model)";
+                    cmd.Parameters.Add(new SqlParameter("@PurchaseDate", computer.PurchaseDate));
+                    cmd.Parameters.Add(new SqlParameter("@DecomissionDate", computer.DecomissionDate));
+                    cmd.Parameters.Add(new SqlParameter("@Make", computer.Make));
+                    cmd.Parameters.Add(new SqlParameter("@Model", computer.Model));
+
+                    int newId = (int)cmd.ExecuteScalar();
+                    computer.Id = newId;
+                    return CreatedAtRoute("GetComputer", new { id = newId }, computer);
+                }
+            }
+        }
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Computer computer)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = @"UPDATE Computer
+        //                                    SET FirstName = @FirstName,
+        //                                        LastName = @LastName,
+        //                                        DepartmentId = @DepartmentId
+        //                                    WHERE Id = @id";
+        //                cmd.Parameters.Add(new SqlParameter("@FirstName", computer.FirstName));
+        //                cmd.Parameters.Add(new SqlParameter("@LastName", computer.LastName));
+        //                cmd.Parameters.Add(new SqlParameter("@DepartmentId", computer.DepartmentId));
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!ComputerExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> Delete([FromRoute] int id)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = @"DELETE FROM Computer WHERE Id = @id";
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!ComputerExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+        //private bool ComputerExists(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                SELECT Id, FirstName, LastName, DepartmentId
+        //                FROM Computer
+        //                WHERE Id = @id";
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //            SqlDataReader reader = cmd.ExecuteReader();
+        //            return reader.Read();
+        //        }
+        //    }
+        //}
     }
 }
