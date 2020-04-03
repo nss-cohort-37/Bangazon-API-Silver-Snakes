7f93301865b08598295272ebb3e42184b34ee41e
Holden Parker <holdenprkr@gmail.com>
2020-04-03 10:21:02 -0500

    departments in progress:

---
+++ BangazonAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> Get([FromQuery] Int32? neighborhoodId)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = "SELECT Id, Name, NeighborhoodId FROM Walker WHERE 1 = 1";
+
+        //            if (neighborhoodId != null)
+        //            {
+        //                cmd.CommandText += " AND NeighborhoodId = @neighborhoodId";
+        //                cmd.Parameters.Add(new SqlParameter("@neighborhoodId", neighborhoodId));
+        //            }
+
+        //            SqlDataReader reader = cmd.ExecuteReader();
+
+        //            List<Walker> walkers = new List<Walker>();
+
+        //            while (reader.Read())
+        //            {
+        //                Walker walker = new Walker
+        //                {
+        //                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                    Name = reader.GetString(reader.GetOrdinal("Name")),
+        //                    NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+        //                };
+
+        //                walkers.Add(walker);
+        //            }
+        //            reader.Close();
+
+        //            return Ok(walkers);
+        //        }
+        //    }
+        //}
+
+        //[HttpGet("{id}", Name = "GetWalkers")]
+        //public async Task<IActionResult> Get(
+        //    [FromRoute] int id,
+        //    [FromQuery] string include)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                    SELECT wr.Id, wr.Name, wr.NeighborhoodId ";
+        //            if (include == "walks")
+        //            {
+        //                cmd.CommandText += ", ws.Id AS WalksId, ws.Date, ws.Duration, ws.WalkerId, ws.DogId ";
+        //            }
+        //            cmd.CommandText += "FROM Walker wr ";
+        //            if (include == "walks")
+        //            {
+        //                cmd.CommandText += "LEFT JOIN Walks ws ON wr.Id = ws.WalkerId ";
+        //            }
+        //            cmd.CommandText += "WHERE wr.Id = @id";
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+        //            SqlDataReader reader = cmd.ExecuteReader();
+
+        //            Walker walker = null;
+
+        //            while (reader.Read())
+        //            {
+        //                if (walker == null)
+        //                {
+        //                    walker = new Walker
+        //                    {
+        //                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                        Name = reader.GetString(reader.GetOrdinal("Name")),
+        //                        NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+        //                        Walks = new List<Walks>()
+        //                    };
+        //                }
+
+        //                if (include == "walks")
+        //                {
+        //                    walker.Walks.Add(new Walks()
+        //                    {
+        //                        Id = reader.GetInt32(reader.GetOrdinal("WalksId")),
+        //                        Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+        //                        Duration = reader.GetInt32(reader.GetOrdinal("Duration")),
+        //                        WalkerId = reader.GetInt32(reader.GetOrdinal("WalkerId")),
+        //                        DogId = reader.GetInt32(reader.GetOrdinal("WalkerId"))
+        //                    });
+        //                }
+        //            }
+        //            reader.Close();
+
+        //            return Ok(walker);
+        //        }
+        //    }
+        //}
+
+        //[HttpPost]
+        //public async Task<IActionResult> Post([FromBody] Walker walker)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"INSERT INTO Walker (Name, NeighborhoodId)
+        //                                    OUTPUT INSERTED.Id
+        //                                    VALUES (@name, @neighborhoodId)";
+        //            cmd.Parameters.Add(new SqlParameter("@name", walker.Name));
+        //            cmd.Parameters.Add(new SqlParameter("@neighborhoodId", walker.NeighborhoodId));
+
+        //            int newId = (int)cmd.ExecuteScalar();
+        //            walker.Id = newId;
+        //            return CreatedAtRoute("GetWalkers", new { id = newId }, walker);
+        //        }
+        //    }
+        //}
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Walker walker)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = @"UPDATE Walker
+        //                                        SET Name = @name,
+        //                                            NeighborhoodId = @neighborhoodId
+        //                                        WHERE Id = @id";
+        //                cmd.Parameters.Add(new SqlParameter("@name", walker.Name));
+        //                cmd.Parameters.Add(new SqlParameter("@neighborhoodId", walker.NeighborhoodId));
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!WalkerExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> Delete([FromRoute] int id)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = @"DELETE FROM Walker WHERE Id = @id";
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!WalkerExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+
+        //private bool WalkerExists(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                    SELECT Id, Name, NeighborhoodId
+        //                    FROM Walker
+        //                    WHERE Id = @id";
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //            SqlDataReader reader = cmd.ExecuteReader();
+        //            return reader.Read();
+        //        }
+        //    }
+        //}
+    }
+}
+
