4321e21a63a2a516cd6e64e4523cd595fda3cb3d
Holden Parker <holdenprkr@gmail.com>
2020-04-03 11:44:00 -0500

    got all of the departments controller functionality working

--- BangazonAPI/Controllers/DepartmentsController.cs
+++ BangazonAPI/Controllers/DepartmentsController.cs
@@ -61,180 +61,146 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        //[HttpGet("{id}", Name = "GetWalkers")]
-        //public async Task<IActionResult> Get(
-        //    [FromRoute] int id,
-        //    [FromQuery] string include)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"
-        //                    SELECT wr.Id, wr.Name, wr.NeighborhoodId ";
-        //            if (include == "walks")
-        //            {
-        //                cmd.CommandText += ", ws.Id AS WalksId, ws.Date, ws.Duration, ws.WalkerId, ws.DogId ";
-        //            }
-        //            cmd.CommandText += "FROM Walker wr ";
-        //            if (include == "walks")
-        //            {
-        //                cmd.CommandText += "LEFT JOIN Walks ws ON wr.Id = ws.WalkerId ";
-        //            }
-        //            cmd.CommandText += "WHERE wr.Id = @id";
-        //            cmd.Parameters.Add(new SqlParameter("@id", id));
-        //            SqlDataReader reader = cmd.ExecuteReader();
+        [HttpGet("{id}", Name = "GetDepartments")]
+        public async Task<IActionResult> Get(
+            [FromRoute] int id,
+            [FromQuery] string include)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT d.Id, d.Name, d.Budget ";
+                    if (include == "employees")
+                    {
+                        cmd.CommandText += ", e.Id AS EmployeeId, e.FirstName, e.LastName, e.DepartmentId, e.Email, e.IsSupervisor, e.ComputerId ";
+                    }
+                    cmd.CommandText += "FROM Department d ";
+                    if (include == "employees")
+                    {
+                        cmd.CommandText += "LEFT JOIN Employee e ON d.Id = e.DepartmentId ";
+                    }
+                    cmd.CommandText += "WHERE d.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
 
-        //            Walker walker = null;
+                    Department department = null;
 
-        //            while (reader.Read())
-        //            {
-        //                if (walker == null)
-        //                {
-        //                    walker = new Walker
-        //                    {
-        //                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
-        //                        Name = reader.GetString(reader.GetOrdinal("Name")),
-        //                        NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
-        //                        Walks = new List<Walks>()
-        //                    };
-        //                }
+                    while (reader.Read())
+                    {
+                        if (department == null)
+                        {
+                            department = new Department
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                                Employees = new List<Employee>()
+                            };
+                        }
 
-        //                if (include == "walks")
-        //                {
-        //                    walker.Walks.Add(new Walks()
-        //                    {
-        //                        Id = reader.GetInt32(reader.GetOrdinal("WalksId")),
-        //                        Date = reader.GetDateTime(reader.GetOrdinal("Date")),
-        //                        Duration = reader.GetInt32(reader.GetOrdinal("Duration")),
-        //                        WalkerId = reader.GetInt32(reader.GetOrdinal("WalkerId")),
-        //                        DogId = reader.GetInt32(reader.GetOrdinal("WalkerId"))
-        //                    });
-        //                }
-        //            }
-        //            reader.Close();
+                        if (include == "employees")
+                        {
+                            department.Employees.Add(new Employee()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")),
+                                IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor")),
+                                ComputerId = reader.GetInt32(reader.GetOrdinal("ComputerId"))
+                            });
+                        }
+                    }
+                    reader.Close();
 
-        //            return Ok(walker);
-        //        }
-        //    }
-        //}
+                    return Ok(department);
+                }
+            }
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Post([FromBody] Walker walker)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"INSERT INTO Walker (Name, NeighborhoodId)
-        //                                    OUTPUT INSERTED.Id
-        //                                    VALUES (@name, @neighborhoodId)";
-        //            cmd.Parameters.Add(new SqlParameter("@name", walker.Name));
-        //            cmd.Parameters.Add(new SqlParameter("@neighborhoodId", walker.NeighborhoodId));
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Department department)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Department (Name, Budget)
+                                            OUTPUT INSERTED.Id
+                                            VALUES (@name, @budget)";
+                    cmd.Parameters.Add(new SqlParameter("@name", department.Name));
+                    cmd.Parameters.Add(new SqlParameter("@budget", department.Budget));
 
-        //            int newId = (int)cmd.ExecuteScalar();
-        //            walker.Id = newId;
-        //            return CreatedAtRoute("GetWalkers", new { id = newId }, walker);
-        //        }
-        //    }
-        //}
+                    int newId = (int)cmd.ExecuteScalar();
+                    department.Id = newId;
+                    return CreatedAtRoute("GetDepartments", new { id = newId }, department);
+                }
+            }
+        }
 
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Walker walker)
-        //{
-        //    try
-        //    {
-        //        using (SqlConnection conn = Connection)
-        //        {
-        //            conn.Open();
-        //            using (SqlCommand cmd = conn.CreateCommand())
-        //            {
-        //                cmd.CommandText = @"UPDATE Walker
-        //                                        SET Name = @name,
-        //                                            NeighborhoodId = @neighborhoodId
-        //                                        WHERE Id = @id";
-        //                cmd.Parameters.Add(new SqlParameter("@name", walker.Name));
-        //                cmd.Parameters.Add(new SqlParameter("@neighborhoodId", walker.NeighborhoodId));
-        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Department department)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Department
+                                                SET Name = @name,
+                                                    Budget = @budget
+                                                WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@name", department.Name));
+                        cmd.Parameters.Add(new SqlParameter("@budget", department.Budget));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
 
-        //                int rowsAffected = cmd.ExecuteNonQuery();
-        //                if (rowsAffected > 0)
-        //                {
-        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
-        //                }
-        //                throw new Exception("No rows affected");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        if (!WalkerExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-        //}
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
 
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> Delete([FromRoute] int id)
-        //{
-        //    try
-        //    {
-        //        using (SqlConnection conn = Connection)
-        //        {
-        //            conn.Open();
-        //            using (SqlCommand cmd = conn.CreateCommand())
-        //            {
-        //                cmd.CommandText = @"DELETE FROM Walker WHERE Id = @id";
-        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+        private bool DepartmentExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT Name, Budget
+                            FROM Department
+                            WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
 
-        //                int rowsAffected = cmd.ExecuteNonQuery();
-        //                if (rowsAffected > 0)
-        //                {
-        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
-        //                }
-        //                throw new Exception("No rows affected");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        if (!WalkerExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-        //}
-
-
-        //private bool WalkerExists(int id)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"
-        //                    SELECT Id, Name, NeighborhoodId
-        //                    FROM Walker
-        //                    WHERE Id = @id";
-        //            cmd.Parameters.Add(new SqlParameter("@id", id));
-
-        //            SqlDataReader reader = cmd.ExecuteReader();
-        //            return reader.Read();
-        //        }
-        //    }
-        //}
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
     }
 }
 
--- BangazonAPI/Models/Department.cs
+++ BangazonAPI/Models/Department.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonAPI.Models;
 
 namespace BangazonAPI.Models
 {
@@ -12,5 +13,7 @@ namespace BangazonAPI.Models
         public string Name { get; set; }
 
         public int Budget { get; set; }
+
+        public List<Employee> Employees { get; set; }
     }
 }
