5996c7fefbc11da8285974831693044df6836af4
Willy <William.g.metcalf@gmail.com>
2020-04-03 14:42:32 -0500

    holden helped me do it correctly

--- BangazonAPI/Controllers/RevenueReportController.cs
+++ BangazonAPI/Controllers/RevenueReportController.cs
@@ -40,20 +40,29 @@ namespace BangazonAPI.Controllers
                 {
                     cmd.CommandText = @"select pt.[Name], pt.Id, SUM(p.Price) as Revenue
                     from OrderProduct op left join Product p on p.Id = op.ProductId
-                    left join ProductType pt on p.ProductTypeId = pt.Id group by pt.[Name], pt.Id ";
+                    full outer join ProductType pt on p.ProductTypeId = pt.Id group by pt.[Name], pt.Id ";
                     SqlDataReader reader = cmd.ExecuteReader();
                     List<RevenueReport> revenueReport = new List<RevenueReport>();
                     RevenueReport reportItem = null;
                     while (reader.Read())
                     {
+                        
+
+
                         reportItem = new RevenueReport
                         {
                             ProductTypeId = reader.GetInt32(reader.GetOrdinal("Id")),
-                            ProductType = reader.GetString(reader.GetOrdinal("Name")),
-                            TotalRevenue = reader.GetDecimal(reader.GetOrdinal("Revenue"))
+                            ProductType = reader.GetString(reader.GetOrdinal("Name"))
+                            
                         };
-
-
+                        if (!reader.IsDBNull(reader.GetOrdinal("Revenue")))
+                        {
+                            reportItem.TotalRevenue = reader.GetDecimal(reader.GetOrdinal("Revenue"));
+                        }
+                        else
+                        {
+                            reportItem.TotalRevenue = 0;
+                        }
 
                         revenueReport.Add(reportItem);
                     }
